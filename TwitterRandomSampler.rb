01144c1ef22926f8f4fdd80fb800683a06398059
Andrew Peterson <AndrewPetersonSoftworks@gmail.com>
2016-01-15 01:44:08 -0500

    Initial Commit - Actually fully functioning, but still the first commit.

---
+++ TwitterRandomSampler.rb
@@ -0,0 +1,35 @@
+require 'net/https'
+require 'uri'
+require 'oauth'
+require 'json'
+require_relative 'environment'
+
+consumer_key = OAuth::Consumer.new(CONSUMER_KEY, CONSUMER_SECRET)
+access_token = OAuth::Token.new(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+address = URI("https://stream.twitter.com/1.1/statuses/sample.json?delimited=length")
+http = Net::HTTP.new address.host, address.port
+http.use_ssl = true
+http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+request = Net::HTTP::Get.new address.request_uri
+request.oauth! http, consumer_key, access_token
+body = ''
+puts "Spooling up the request. This might take a second."
+http.request request do |response|
+	response.read_body do |chunk|
+		body += chunk
+		num_bytes = body.split("\n").first.to_i
+		body = body.split("\n")[1..-1].join("\n")
+		json_blob = body.slice 0, num_bytes
+		if num_bytes > 0
+			parsed = JSON.parse(json_blob)
+			if parsed["text"] != nil
+				puts parsed["screen_name"]
+				puts parsed["text"]
+				puts parsed["created_at"]
+				sleep 1
+			end
+		end
+	end
+end
+
