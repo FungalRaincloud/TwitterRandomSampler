fc63cf4de6a257894fdae63547fa0356d01ecb29
Andrew Peterson <AndrewPetersonSoftworks@gmail.com>
2016-01-31 15:08:42 -0500

    Fool around with formatting the tweets

--- TwitterRandomSampler.rb
+++ TwitterRandomSampler.rb
@@ -14,21 +14,26 @@ http.verify_mode = OpenSSL::SSL::VERIFY_PEER
 request = Net::HTTP::Get.new address.request_uri
 request.oauth! http, consumer_key, access_token
 body = ''
-puts "Spooling up the request. This might take a second."
-http.request request do |response|
-  response.read_body do |chunk|
-    body += chunk
-    num_bytes = body.split("\n").first.to_i
-    body = body.split("\n")[1..-1].join("\n")
-    json_blob = body.slice 0, num_bytes
-    if num_bytes > 0
-      parsed = JSON.parse(json_blob)
-      if parsed["text"] != nil
-        puts parsed["user"]["screen_name"]
-        puts parsed["text"]
-        puts parsed["created_at"]
-        sleep 1
+retries = 5
+begin
+  puts "Spooling up the request. This might take a second.\n"
+  http.request request do |response|
+    response.read_body do |chunk|
+      body += chunk
+      num_bytes = body.split("\n").first.to_i
+      body = body.split("\n")[1..-1].join("\n")
+      json_blob = body.slice 0, num_bytes
+      if num_bytes > 0
+        parsed = JSON.parse(json_blob)
+        if parsed["text"] != nil
+          puts "#{parsed["user"]["screen_name"]}\t\t#{parsed["created_at"]}\n#{parsed["text"]}\n\n\n"
+          sleep 1
+        end
       end
     end
   end
+rescue EOFError
+  puts "Stream Lost. Retries remaining: #{retries-=1}"
+  sleep 5
+  retry if !retries.zero?
 end
