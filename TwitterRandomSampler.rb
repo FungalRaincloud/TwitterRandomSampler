46128ea630e9e081100709519fc7eddafd8bbb3b
Andrew Peterson <AndrewPetersonSoftworks@gmail.com>
2016-02-02 12:44:07 -0500

    Add functionality to pause stream, and quit gracefully - rough version

--- TwitterRandomSampler.rb
+++ TwitterRandomSampler.rb
@@ -4,6 +4,32 @@ require 'oauth'
 require 'json'
 require_relative 'environment'
 
+PAUSE_KEYS = ' '
+QUIT_KEYS = 'qQ'
+@paused = false
+
+def get_key
+  begin
+    system('stty raw -echo') 
+    char = (STDIN.read_nonblock(1).ord rescue 0) # 0 is the ASCII null character.
+  ensure
+    system('stty -raw echo')
+  end
+  return char.chr
+end
+
+def respond_to_keypresses
+  keypress = get_key
+  pause if PAUSE_KEYS.include? keypress
+  exit if QUIT_KEYS.include? keypress
+end
+def pause
+  @paused = !@paused
+  while @paused
+    respond_to_keypresses
+  end
+end
+
 consumer_key = OAuth::Consumer.new(CONSUMER_KEY, CONSUMER_SECRET)
 access_token = OAuth::Token.new(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
@@ -16,7 +42,7 @@ request.oauth! http, consumer_key, access_token
 body = ''
 retries = 5
 begin
-  puts "Spooling up the request. This might take a second.\n"
+  puts "Spooling up the request. This might take a second.\n\n"
   http.request request do |response|
     response.read_body do |chunk|
       body += chunk
@@ -26,7 +52,10 @@ begin
       if num_bytes > 0
         parsed = JSON.parse(json_blob)
         if parsed["text"] != nil
+          # TODO: maybe use curses to make this a little cleaner/easier to read?
+          #
           puts "#{parsed["user"]["screen_name"]}\t\t#{parsed["created_at"]}\n#{parsed["text"]}\n\n\n"
+          respond_to_keypresses
           sleep 1
         end
       end
@@ -35,5 +64,7 @@ begin
 rescue EOFError
   puts "Stream Lost. Retries remaining: #{retries-=1}"
   sleep 5
-  retry if !retries.zero?
+  if !retries.zero?
+    retry
+  end
 end
