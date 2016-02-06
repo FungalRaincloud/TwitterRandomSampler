e7af75ec9c4f4b3ea2f4476769c682c9f1777e56
Andrew Peterson <AndrewPetersonSoftworks@gmail.com>
2016-02-06 14:47:41 -0500

    Create class Twitter_Connector, move related code to it.

--- TwitterRandomSampler.rb
+++ TwitterRandomSampler.rb
@@ -3,14 +3,17 @@ require 'uri'
 require 'oauth'
 require 'json'
 require_relative 'environment'
+require_relative 'TwitterConnector'
 
-PAUSE_KEYS = ' '
+PAUSE_KEYS = ' pP'
 QUIT_KEYS = 'qQ'
+ARG_KEYS = 'cC'
+HELP_KEYS = 'hH?'
 @paused = false
 
 def get_key
   begin
-    system('stty raw -echo') 
+    system('stty raw -echo')
     char = (STDIN.read_nonblock(1) rescue "\0") # '\0' is the ASCII null character.
   ensure
     system('stty -raw echo')
@@ -21,8 +24,35 @@ end
 def respond_to_keypresses
   keypress = get_key
   pause if PAUSE_KEYS.include? keypress
+  change_args if ARG_KEYS.include? keypress
+  help if HELP_KEYS.include? keypress
   exit if QUIT_KEYS.include? keypress
 end
+
+def help
+  puts "\n'#{HELP_KEYS}' - display this help message"
+  puts "'#{ARG_KEYS}' - change twitter language/filter/stream type"
+  puts "'#{QUIT_KEYS}' - quit"
+  puts "'#{PAUSE_KEYS}' - pause"
+  puts "Press space to continue execution."
+  pause if !@paused
+end
+
+def change_args
+  print "\nstream <sample.json>:"
+  stream = gets.chomp
+  stream = 'sample.json' if stream.empty?
+  print "search filter (space delimited) <none>:"
+  #TODO: change to a url encoder, add same to stream and language
+  filter = gets.chomp.gsub('#', "%27").gsub(' ', '%20')
+  print "language <en>:"
+  language = gets.chomp
+  #TODO: make check for valid language by declaring a constant in Twitter_Connector?
+  language = 'en' if language.empty?
+  puts "stream: #{stream}, filter: #{filter}, language: #{language}"
+  @tc = Twitter_Connector.new stream_source: stream, filter: filter, language: language
+end
+
 def pause
   @paused = !@paused
   while @paused
@@ -30,41 +60,8 @@ def pause
   end
 end
 
-consumer_key = OAuth::Consumer.new(CONSUMER_KEY, CONSUMER_SECRET)
-access_token = OAuth::Token.new(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-
-address = URI("https://stream.twitter.com/1.1/statuses/sample.json?delimited=length&language=#{ARGV.empty? ? '' : ARGV[0]}")
-http = Net::HTTP.new address.host, address.port
-http.use_ssl = true
-http.verify_mode = OpenSSL::SSL::VERIFY_PEER
-request = Net::HTTP::Get.new address.request_uri
-request.oauth! http, consumer_key, access_token
-body = ''
-retries = 5
-begin
-  puts "Spooling up the request. This might take a second.\n\n"
-  http.request request do |response|
-    response.read_body do |chunk|
-      body += chunk
-      num_bytes = body.split("\n").first.to_i
-      body = body.split("\n")[1..-1].join("\n")
-      json_blob = body.slice 0, num_bytes
-      if num_bytes > 0
-        parsed = JSON.parse(json_blob)
-        if parsed["text"] != nil
-          # TODO: maybe use curses to make this a little cleaner/easier to read?
-          #
-          puts "#{parsed["user"]["screen_name"]}\t\t#{parsed["created_at"]}\n#{parsed["text"]}\n\n\n"
-          respond_to_keypresses
-          sleep 1
-        end
-      end
-    end
-  end
-rescue EOFError
-  puts "Stream Lost. Retries remaining: #{retries-=1}"
-  sleep 5
-  if !retries.zero?
-    retry
-  end
+@tc = Twitter_Connector.new
+while true do
+  #@tc.connect
+  respond_to_keypresses
 end
